063fdded8b982e48926a5ec411c33eeb592f7668
Kyupang <80692478+Kyupang@users.noreply.github.com>
2023-10-11 16:51:00 +0900

    [level 3] Title: 가장 먼 노드, Time: 60.93 ms, Memory: 99 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -0,0 +1,52 @@
+import java.util.*;
+class Solution {
+
+	ArrayList<Integer> a[];
+	boolean[] visited;
+	int[] dist;
+	
+	public int solution(int n, int[][] edge) {
+		a = new ArrayList[n+1];
+		visited = new boolean[n+1];
+		dist = new int[n+1];
+		int answer = 0;
+		
+		Arrays.fill(dist, -1);
+		dist[1] = 0;
+		for(int i=1 ; i<=n ; i++) {
+			a[i] = new ArrayList<Integer>();
+		}
+		
+		for(int i=0 ; i< edge.length ; i++) {
+			int start = edge[i][0];
+			int end = edge[i][1];
+			
+			a[start].add(end);
+			a[end].add(start);
+		}
+		
+		bfs(1);
+		
+		int maxDist = Arrays.stream(dist).max().getAsInt();
+		answer = (int) Arrays.stream(dist).filter(d -> d == maxDist).count();
+
+		return answer;
+	}
+
+	private void bfs(int node) {
+		Queue<Integer> queue = new LinkedList<Integer>();
+		visited[node] = true;
+		queue.add(node);
+		
+		while(!queue.isEmpty()) {
+			int now = queue.poll();
+			for(int next : a[now]) {
+				if(!visited[next]) {
+					dist[next] = dist[now]+1;
+					visited[next] = true;
+					queue.add(next);
+				}
+			}
+		}
+    }
+}
