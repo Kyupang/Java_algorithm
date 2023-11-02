a4aee4b36065528922092910c416d60b3680c33d
Kyupang <80692478+Kyupang@users.noreply.github.com>
2023-11-02 18:25:28 +0900

    [Silver IV] Title: 설탕 배달, Time: 204 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,26 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-
+	private static final int FIVE_KILOGRAM_SUGAR = 5;
+	private static final int THREE_KILOGRAM_SUGAR = 3;
+	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int nKilogram = scanner.nextInt();
-
-		int answer = 0;
-		while (nKilogram >= 0) {
-			if (nKilogram % 5 == 0) {
-				answer += nKilogram / 5;
+		int totalSugarQuantity = scanner.nextInt();
+		int bag = 0;
+		
+		while (totalSugarQuantity >= 0) {
+			if (totalSugarQuantity % FIVE_KILOGRAM_SUGAR == 0) {
+				bag += totalSugarQuantity / FIVE_KILOGRAM_SUGAR;
 				break;
 			}
-			nKilogram -= 3;
-			answer++;
+			totalSugarQuantity -= THREE_KILOGRAM_SUGAR;
+			bag++; // 3kg 카운트 증가
 		}
 
-		if (nKilogram < 0) {
-			answer = -1;
+		if (totalSugarQuantity < 0) {
+			bag = -1;
 		}
 
-		System.out.println(answer);
+		System.out.println(bag);
 	}
 
 }
