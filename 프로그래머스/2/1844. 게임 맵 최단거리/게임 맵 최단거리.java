0aab798f4648eace264e24c38c8b01dcc8525768
Kyupang <80692478+Kyupang@users.noreply.github.com>
2024-03-13 02:09:01 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.49 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,42 +1,40 @@
 import java.util.*;
 class Solution {
-    static int[] dr = {0,1,0,-1};
-    static int[] dc = {1,0,-1,0};
-    static boolean visited[][];
-
-    public int solution(int[][] maps) {  //처음에 그래프를 받습니다.
-        int answer = -1;
-        int n = maps.length;
-        int m = maps[0].length;
-        visited = new boolean[n][m];
+    static int[] dr = {0, 1, 0, -1};
+    static int[] dc = {1, 0, -1, 0};
+    
+    public int solution(int[][] maps) { 
+        int rowSize = maps.length;
+        int colSize = maps[0].length;
         
-        BFS(0,0,n,m,maps);
+        boolean[][] visited = new boolean[rowSize][colSize];
         
+        findRoute(visited, maps);
         
-        return !visited[n-1][m-1] ? -1 : maps[n-1][m-1];
+        return !visited[rowSize - 1][colSize - 1] ? -1 : maps[rowSize - 1][colSize - 1]; 
     }
     
-    private static void BFS(int r, int c, int N, int M, int[][] maps){
-        Queue<int[]> q = new LinkedList<>();
-        visited[r][c] = true;
-        q.offer(new int[] {r,c});
+    private static void findRoute(boolean visited[][], int[][] maps) {
+        Queue<int[]> que = new LinkedList<>();
+        que.offer(new int[] {0, 0});
+        visited[0][0] = true;
         
-        while(!q.isEmpty()){
-            int[] now = q.poll();
+        while (!que.isEmpty()) {
+            int[] now = que.poll();
             
-            for(int k = 0; k< 4 ; k++){
+            for (int k = 0; k < 4; k++) {
                 int nr = now[0] + dr[k];
                 int nc = now[1] + dc[k];
                 
-                if(nr>=0 && nc>=0 && nr< N &&nc<M){
-                    if(!visited[nr][nc] && maps[nr][nc] !=0){
-                        visited[nr][nc] = true;
-                        maps[nr][nc] = maps[now[0]][now[1]]+1;
-                        q.offer(new int[] {nr,nc});
-                    }
-                }
+                if (nr < 0 || nc < 0 || nr >= maps.length || nc >= maps[0].length)
+                    continue;                
+                if (visited[nr][nc] || maps[nr][nc] == 0)
+                    continue;
+                
+                visited[nr][nc] = true;
+                maps[nr][nc] = maps[now[0]][now[1]] + 1;
+                que.offer(new int[] {nr, nc});
             }
         }
-    } 
-    
+    }
 }
